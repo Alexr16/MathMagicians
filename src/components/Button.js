c367c8b427e979471f7ce25e4290ad45b39e799b
Alexr16 <j.alex-1996@hotmail.com>
2022-04-21 13:12:56 -0500

    Refactor from class to function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,30 @@
-const Button = (Click, KeyPress, Name, val, lab) => {
-  const { onClick } = Click;
-  const { value } = val;
-  const { label } = lab;
-  const { className } = Name;
-  const { onKeyPress } = KeyPress;
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  const {
+    Click, KeyPress, Name, val, lab,
+  } = props;
+
   return (
     <div
-      onClick={() => onClick}
-      onKeyPress={onKeyPress}
+      onClick={Click}
+      onKeyPress={KeyPress}
       tabIndex="0"
-      className={`btn ${className}`}
-      data-value={value}
+      className={`btn ${Name}`}
+      data-value={val}
       role="button"
     >
-      {label}
+      {lab}
     </div>
   );
 };
 
-
+Button.propTypes = {
+  Click: PropTypes.func.isRequired,
+  Name: PropTypes.string.isRequired,
+  val: PropTypes.string.isRequired,
+  lab: PropTypes.string.isRequired,
+  KeyPress: PropTypes.func.isRequired,
+};
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -23,25 +23,25 @@ const Calculator = () => {
     <div className="container">
       <Display data={next || total} />
       <div div className="wrapper">
-        <Button Click={handleClick('AC')} KeyPress={onKeyPress} Name="gray" lab="AC" val="AC" />
-        <Button Click={handleClick('+/-')} KeyPress={onKeyPress} Name="gray" lab="+/-" val="+/-" />
-        <Button Click={handleClick('%')} KeyPress={onKeyPress} Name="gray" lab="%" val="%" />
-        <Button Click={handleClick('÷')} KeyPress={onKeyPress} Name="orange" lab="÷" val="÷" />
-        <Button Click={handleClick('7')} KeyPress={onKeyPress} Name="gray" lab="7" val="7" />
-        <Button Click={handleClick('8')} KeyPress={onKeyPress} Name="gray" lab="8" val="8" />
-        <Button Click={handleClick('9')} KeyPress={onKeyPress} Name="gray" lab="9" val="9" />
-        <Button Click={handleClick('X')} KeyPress={onKeyPress} Name="orange" lab="x" val="x" />
-        <Button Click={handleClick('4')} KeyPress={onKeyPress} Name="gray" lab="4" val="4" />
-        <Button Click={handleClick('5')} KeyPress={onKeyPress} Name="gray" lab="5" val="5" />
-        <Button Click={handleClick('6')} KeyPress={onKeyPress} Name="gray" lab="6" val="6" />
-        <Button Click={handleClick('-')} KeyPress={onKeyPress} Name="orange" lab="-" val="-" />
-        <Button Click={handleClick('1')} KeyPress={onKeyPress} Name="gray" lab="1" val="1" />
-        <Button Click={handleClick('2')} KeyPress={onKeyPress} Name="gray" lab="2" val="2" />
-        <Button Click={handleClick('3')} KeyPress={onKeyPress} Name="gray" lab="3" val="3" />
-        <Button Click={handleClick('+')} KeyPress={onKeyPress} Name="orange" lab="+" val="+" />
-        <Button Click={handleClick('0')} KeyPress={onKeyPress} Name="gray zero" lab="0" val="0" />
-        <Button Click={handleClick('.')} KeyPress={onKeyPress} Name="gray" lab="." val="." />
-        <Button Click={handleClick('=')} KeyPress={onKeyPress} Name="orange" lab="=" val="=" />
+        <Button Click={() => handleClick('AC')} KeyPress={() => onKeyPress()} Name="gray" lab="AC" val="AC" />
+        <Button Click={() => handleClick('+/-')} KeyPress={() => onKeyPress()} Name="gray" lab="+/-" val="+/-" />
+        <Button Click={() => handleClick('%')} KeyPress={() => onKeyPress()} Name="gray" lab="%" val="%" />
+        <Button Click={() => handleClick('÷')} KeyPress={() => onKeyPress()} Name="orange" lab="÷" val="÷" />
+        <Button Click={() => handleClick('7')} KeyPress={() => onKeyPress()} Name="gray" lab="7" val="7" />
+        <Button Click={() => handleClick('8')} KeyPress={() => onKeyPress()} Name="gray" lab="8" val="8" />
+        <Button Click={() => handleClick('9')} KeyPress={() => onKeyPress()} Name="gray" lab="9" val="9" />
+        <Button Click={() => handleClick('X')} KeyPress={() => onKeyPress()} Name="orange" lab="x" val="x" />
+        <Button Click={() => handleClick('4')} KeyPress={() => onKeyPress()} Name="gray" lab="4" val="4" />
+        <Button Click={() => handleClick('5')} KeyPress={() => onKeyPress()} Name="gray" lab="5" val="5" />
+        <Button Click={() => handleClick('6')} KeyPress={() => onKeyPress()} Name="gray" lab="6" val="6" />
+        <Button Click={() => handleClick('-')} KeyPress={() => onKeyPress()} Name="orange" lab="-" val="-" />
+        <Button Click={() => handleClick('1')} KeyPress={() => onKeyPress()} Name="gray" lab="1" val="1" />
+        <Button Click={() => handleClick('2')} KeyPress={() => onKeyPress()} Name="gray" lab="2" val="2" />
+        <Button Click={() => handleClick('3')} KeyPress={() => onKeyPress()} Name="gray" lab="3" val="3" />
+        <Button Click={() => handleClick('+')} KeyPress={() => onKeyPress()} Name="orange" lab="+" val="+" />
+        <Button Click={() => handleClick('0')} KeyPress={() => onKeyPress()} Name="gray zero" lab="0" val="0" />
+        <Button Click={() => handleClick('.')} KeyPress={() => onKeyPress()} Name="gray" lab="." val="." />
+        <Button Click={() => handleClick('=')} KeyPress={() => onKeyPress()} Name="orange" lab="=" val="=" />
       </div>
     </div>
   );
