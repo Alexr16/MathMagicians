ee3fef56781ea86d1483095f4b26a6db6e029810
Alexr16 <j.alex-1996@hotmail.com>
2022-04-21 09:48:47 -0500

    Refactor App.js from a class to a function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import React, { PureComponent } from 'react';
 import Calculator from './components/Calculator';
 
-class App extends PureComponent {
-  render() {
-    return (
-      <div className="calc-container">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="calc-container">
+    <Calculator />
+  </div>
+);
 
 export default App;
