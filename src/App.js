03869f0d6795bd1e438f3643b6452500bbfc8e89
Alexr16 <j.alex-1996@hotmail.com>
2022-04-26 13:17:48 -0500

    Add routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
 const App = () => (
-  <div className="calc-container">
-    <Calculator />
-  </div>
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/calculator"
+        element={(
+          <div className="calc-container">
+            <Calculator />
+          </div>
+    )}
+      />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
 );
 
 export default App;
