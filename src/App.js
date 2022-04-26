961b3d6ab69f26a85dde32016db93e533dfa3008
Alexr16 <j.alex-1996@hotmail.com>
2022-04-26 13:08:57 -0500

    Test react-route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
-import React, { PureComponent } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
-class App extends PureComponent {
-  render() {
-    return (
-      <div className="calc-container">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/calculator"
+        element={(
+          <div className="calc-container">
+            <Calculator />
+          </div>
+    )}
+      />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 
 export default App;
