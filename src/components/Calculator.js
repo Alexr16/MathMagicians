ee2aed3005ee65e0d20159b258460e20e460bbc3
Alexr16 <j.alex-1996@hotmail.com>
2022-04-26 13:42:51 -0500

    Add Calculator page content

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,30 +20,35 @@ const Calculator = () => {
   };
 
   return (
-    <div className="container">
-      <div className="display">{next || total}</div>
-      <div className="wrapper">
-        <div role="button" tabIndex="0" onClick={() => handleClick('AC')} onKeyPress={onKeyPress} className="btn gray" value="AC">AC</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('+/-')} onKeyPress={onKeyPress} className="btn gray" value="+/-">+/-</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('%')} onKeyPress={onKeyPress} className="btn gray" value="%">%</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('÷')} onKeyPress={onKeyPress} className="btn orange" value="÷">÷</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('7')} onKeyPress={onKeyPress} className="btn gray" value="7">7</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('8')} onKeyPress={onKeyPress} className="btn gray" value="8">8</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('9')} onKeyPress={onKeyPress} className="btn gray" value="9">9</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('x')} onKeyPress={onKeyPress} className="btn orange" value="x">x</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('4')} onKeyPress={onKeyPress} className="btn gray" value="4">4</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('5')} onKeyPress={onKeyPress} className="btn gray" value="5">5</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('6')} onKeyPress={onKeyPress} className="btn gray" value="6">6</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('-')} onKeyPress={onKeyPress} className="btn orange" value="-">-</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('1')} onKeyPress={onKeyPress} className="btn gray" value="1">1</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('2')} onKeyPress={onKeyPress} className="btn gray" value="2">2</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('3')} onKeyPress={onKeyPress} className="btn gray" value="3">3</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('+')} onKeyPress={onKeyPress} className="btn orange" value="+">+</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('0')} onKeyPress={onKeyPress} className="btn gray zero" value="0">0</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('.')} onKeyPress={onKeyPress} className="btn gray" value=".">.</div>
-        <div role="button" tabIndex="0" onClick={() => handleClick('=')} onKeyPress={onKeyPress} className="btn orange" value="=">=</div>
+    <>
+      <div>
+        <h2>Let´s do some math!</h2>
       </div>
-    </div>
+      <div className="container">
+        <div className="display">{next || total}</div>
+        <div className="wrapper">
+          <div role="button" tabIndex="0" onClick={() => handleClick('AC')} onKeyPress={onKeyPress} className="btn gray" value="AC">AC</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('+/-')} onKeyPress={onKeyPress} className="btn gray" value="+/-">+/-</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('%')} onKeyPress={onKeyPress} className="btn gray" value="%">%</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('÷')} onKeyPress={onKeyPress} className="btn orange" value="÷">÷</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('7')} onKeyPress={onKeyPress} className="btn gray" value="7">7</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('8')} onKeyPress={onKeyPress} className="btn gray" value="8">8</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('9')} onKeyPress={onKeyPress} className="btn gray" value="9">9</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('x')} onKeyPress={onKeyPress} className="btn orange" value="x">x</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('4')} onKeyPress={onKeyPress} className="btn gray" value="4">4</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('5')} onKeyPress={onKeyPress} className="btn gray" value="5">5</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('6')} onKeyPress={onKeyPress} className="btn gray" value="6">6</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('-')} onKeyPress={onKeyPress} className="btn orange" value="-">-</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('1')} onKeyPress={onKeyPress} className="btn gray" value="1">1</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('2')} onKeyPress={onKeyPress} className="btn gray" value="2">2</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('3')} onKeyPress={onKeyPress} className="btn gray" value="3">3</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('+')} onKeyPress={onKeyPress} className="btn orange" value="+">+</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('0')} onKeyPress={onKeyPress} className="btn gray zero" value="0">0</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('.')} onKeyPress={onKeyPress} className="btn gray" value=".">.</div>
+          <div role="button" tabIndex="0" onClick={() => handleClick('=')} onKeyPress={onKeyPress} className="btn orange" value="=">=</div>
+        </div>
+      </div>
+    </>
   );
 };
 
